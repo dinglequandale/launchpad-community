8410bd47dae4d471f01eb3e9cbd8da4003c3581d
dinglequandale <konradatittel@gmail.com>
2024-06-12 10:34:34 -0500

    basic landing page styled, button onclick routing implemented between landing page and sign up page

--- launchpad/src/main.jsx
+++ launchpad/src/main.jsx
@@ -2,9 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Signup from './pages/authentication/signup/Signup.jsx'
+
+
+const router = createBrowserRouter([
+  {
+  path: '/',
+  element: <App/>,
+  errorElement: <div>Sorry pookie! We got an error on our hands ... Try reload!</div>
+},
+{
+  path: '/Signup',
+  element: <Signup/>,
+}
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router = {router} />
   </React.StrictMode>,
 )
