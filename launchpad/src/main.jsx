1fcb42d7b620ef902ea6ea2589837942081ba0b9
dinglequandale <konradatittel@gmail.com>
2024-06-12 18:16:52 -0500

    core authentication added with Auth0, TODO is format it properly and fix sign in / sign out issue

--- launchpad/src/main.jsx
+++ launchpad/src/main.jsx
@@ -4,7 +4,12 @@ import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Signup from './pages/authentication/signup/Signup.jsx'
+import {Auth0Provider} from '@auth0/auth0-react';
+import authConfig from '/auth_config.json';
 
+
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+const domain = import.meta.env.VITE_AUTH0_DOMAIN;
 
 const router = createBrowserRouter([
   {
@@ -21,6 +26,12 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router = {router} />
+    <Auth0Provider 
+    domain = {domain}
+    clientId={clientId}
+    redirectUri = {window.location.origin}>
+      <RouterProvider router = {router} /> 
+    </Auth0Provider>
+  
   </React.StrictMode>,
 )
--- launchpad/src/pages/landing_page/LandingPage.jsx
+++ launchpad/src/pages/landing_page/LandingPage.jsx
@@ -3,9 +3,7 @@ import { useNavigate } from "react-router-dom"
 import "./landingpage.css"
 
 export default function LandingPage() {
-
   const navigate = useNavigate();
-
   return (
     <div className="background">
       <div className="landingPage">
