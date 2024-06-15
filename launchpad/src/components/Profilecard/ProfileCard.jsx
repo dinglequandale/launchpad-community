298037ce357b7651a135daafd10c24bc4516f31c
dinglequandale <konradatittel@gmail.com>
2024-06-15 01:14:40 -0500

    Profile card view for user on click styled

---
+++ launchpad/src/components/Profilecard/ProfileCard.jsx
@@ -0,0 +1,38 @@
+import './profilecard.css';
+import React from 'react';
+import { VscAccount } from "react-icons/vsc";
+import { SlLink } from "react-icons/sl";   
+import { IoCloseOutline } from "react-icons/io5";
+import SideNav from '../Sidenav/SideNav';
+
+export default function ProfileCard({userData, onClose}) {
+    return(
+        <div className='profileCardView'>
+            <SideNav/>
+            <div className='profileCardContainer'>
+                <div className='profileCard'>
+                    <IoCloseOutline className='close' onClick={onClose}/>
+                    <div className='basicInfo'>
+                        <div>
+                            <VscAccount size = {80} className='cardPfp'/>
+                        </div>
+                        <div className='cardNameDescription'>
+                            <span className='cardName'>Name Tittel</span>
+                            <span className='cardDescription'> Short description</span>
+                        </div>   
+                    </div>
+                    <div className='userInfo'>
+                        <span>Current position (if applicable): ...</span>
+                        <span>Expertise (if applicable): ... </span>
+                    </div>
+                    <button className='cardbtnConnect'> 
+                        <div>
+                            <SlLink size={15}/> Connect
+                        </div> 
+                    </button>
+                    <span className='cardResume'>Resume:</span> 
+                </div>
+            </div>
+        </div>
+    )
+}
