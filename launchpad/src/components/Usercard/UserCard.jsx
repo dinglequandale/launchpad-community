620a67f2f6fe42fc912297ea4b061458ee4ba7e1
dinglequandale <konradatittel@gmail.com>
2024-06-16 13:23:24 -0500

    banner component for profile card finalized

--- launchpad/src/components/Usercard/UserCard.jsx
+++ launchpad/src/components/Usercard/UserCard.jsx
@@ -1,31 +1,34 @@
 import "./usercard.css";
 import { VscAccount } from "react-icons/vsc";
 import { SlLink } from "react-icons/sl";   
+import ConnectionBanner from "../Connectionbanner/ConnectionBanner";
+import { useState } from "react";
 
 export default function ProfileCard({userData}) {
+    const [bannerVisibility, setBannerVisibility] = useState(false);
+    console.log(bannerVisibility);
     return(
-        <>
-                <div className='networkProfileCard'>
-                    <span style={{position: "relative", right: "3%", fontSize: "smaller"}} className="seeProfile">See Profile</span>
-                    <div className='basicInfo'>
-                        <div>
-                            <VscAccount size = {80} className='cardPfp'/>
-                        </div>
-                        <div className='cardNameDescription'>
-                            <span className='cardName'>Name Tittel</span>
-                            <span className='cardDescription'> Short description</span>
-                        </div>   
-                    </div>
-                    <div className='userInfo'>
-                        <span>Current position (if applicable): ...</span>
-                        <span>Expertise (if applicable): ... </span>
-                    </div>
-                    <button className='userCardBtnConnect'> 
-                        <div>
-                            <SlLink size={15}/> Connect
-                        </div> 
-                    </button>
+        <div className='networkProfileCard'>
+            {bannerVisibility && <ConnectionBanner/>}
+            <span style={{position: "absolute", right: "3%", fontSize: "smaller"}} className="seeProfile">See Profile</span>
+            <div className='basicInfo'>
+                <div>
+                    <VscAccount size = {80} className='cardPfp'/>
                 </div>
-        </>   
+                <div className='cardNameDescription'>
+                    <span className='cardName'>Name Tittel</span>
+                    <span className='cardDescription'> Short description</span>
+                </div>   
+            </div>
+            <div className='userInfo'>
+                <span>Current position (if applicable): ...</span>
+                <span>Expertise (if applicable): ... </span>
+            </div>
+            <button className='userCardBtnConnect'> 
+                <div>
+                    <SlLink size={15}/> Connect
+                </div> 
+            </button>
+        </div>
     )
 }
--- launchpad/src/pages/Network/UserNetwork.jsx
+++ launchpad/src/pages/Network/UserNetwork.jsx
@@ -1,5 +1,6 @@
 import "./user_network.css";
 import UserCard from "../../components/Usercard/UserCard";
+import ConnectionBanner from "../../components/Connectionbanner/ConnectionBanner";
 
 export default function UserNetwork() {
   return (
