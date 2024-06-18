97acca8d4886b11d310f532aed014fcb7c170ee5
dinglequandale <konradatittel@gmail.com>
2024-06-18 16:46:25 -0500

    small bug fixes

--- launchpad/src/main.jsx
+++ launchpad/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import SideNav_Test from './pages/temp_sidenavTesting/SideNav_Test.jsx';
 import {Auth0Provider} from '@auth0/auth0-react';
 import UserNetwork from './pages/Network/UserNetwork.jsx';
 import UserSchool from './pages/Onboarding/Initialphase/Userschool/UserSchool.jsx';
@@ -15,11 +14,6 @@ const router = createBrowserRouter([
   {
   path: '/',
   element: <App/>,
-  errorElement: <div>Sorry pookie! We got an error on our hands ... Try reload!</div>
-},
-{
-  path: '/OnBoarding_1',
-  element: <SideNav_Test/>,
   errorElement: <div>Sorry pookie! We got an error on our hands ... Try reload!</div>
 },
 {
