9d8049e9b23180594bea5121b01a5bcb44717c4e
gallo-json <gallojsantiago@gmail.com>
2024-07-03 18:47:47 -0500

    not working

--- launchpad/src/pages/Onboarding/HighSchooler/HighSchooler.jsx
+++ launchpad/src/pages/Onboarding/HighSchooler/HighSchooler.jsx
@@ -1,73 +1,142 @@
-const highSchoolQuestionsConfig = [
-    // Page 1
-    {
-        id: "whatSchool",
-        text: "What school do you go to?",
-        type: "select",
-        options: ["The Awty International School"],
-        page: 1,
-    },
-    {
-        id: "graduationYear",
-        text: "What year do you graduate?",
-        type: "select",
-        options: [1999, 2008],
-        page: 1,
-    },
-    {
-        id: "whatSection",
-        text: "Are you part of the French or International Section?",
-        type: "select",
-        options: ["French", "International"],
-        page: 1,
-    },
+import React from 'react';
+import OnboardingDropdown from '../../../components/OnboardingDropdown/OnboardingDropdown';
 
-    // Page 2
-    {
-        id: "dreamCareer",
-        text: "What is your dream career field? (Select up to 4)",
-        type: "select",
-        options: [
-            "Consumer Electronics",
-            "Consumer Goods",
-            "Consumer Services",
-            "Cosmetics",
-            "Business",
-            "Education",
-            "Healthcare",
-            "Technology"
-        ],
-        page: 2,
-    },
 
-    // Page 3
+export default HighSchooler = () => {
+    const numOfSections = 3;
+    const [currentPage, setCurrentPage] = useState(1);
+    const [selectedOptions, setSelectedOptions] = useState({});
+
+    const handleDropdownChange = (id, value) => {
+        setSelectedOptions(prevState => ({
+        ...prevState,
+        [id]: value,
+        }));
+    };
+
+    const renderPage = () => {
+        switch (currentPage) {
+        case 1:
+            return <Page1 selectedOptions={selectedOptions} handleChange={handleDropdownChange} />;
+        case 2:
+            return <Page2 selectedOptions={selectedOptions} handleChange={handleDropdownChange} />;
+        case 3:
+            return <Page3 selectedOptions={selectedOptions} handleChange={handleDropdownChange} />;
+        default:
+            return null;
+        }
+};
+
+return (
+    <div>
+    <ProgressBar
+        numOfSections={numOfSections}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        showLast={true}
+    />
+    {renderPage()}
+    </div>
+);
+};
+  
+const Page1 = ({ selectedOptions, handleChange }) => {
+  const questions = [
     {
-        id: "collegeDecision",
-        text: "Have you decided what college you will attend after highschool?",
-        type: "select",
-        options: ["Yes", "No"],
-        page: 3
+      id: "whatSchool",
+      text: "What school do you go to?",
+      options: ["The Awty International School"],
     },
     {
-        id: "collegeInterests",
-        text: "What colleges are you interested in attending after highschool?",
-        type: "select",
-        options: [
-            "Harvard",
-            "Yale",
-            "UT Austin"
-        ],
-        page: 3  
+      id: "graduationYear",
+      text: "What year do you graduate?",
+      options: [1999, 2008],
     },
     {
-        id: "collegeAttending",
-        text: "What college will you be attending?",
-        type: "select",
-        options: [
-            "Harvard",
-            "Yale",
-            "UT Austin"
-        ],
-        page: 3  
-    }
-]
+      id: "whatSection",
+      text: "Are you part of the French or International Section?",
+      options: ["French", "International"],
+    },
+  ];
+
+  return (
+    <div>
+      {questions.map((question) => (
+        <OnboardingDropdown
+          key={question.id}
+          question={question.text}
+          options={question.options}
+          selectedOption={selectedOptions[question.id]}
+          onChange={(e) => handleChange(question.id, e.target.value)}
+        />
+      ))}
+    </div>
+  );
+};
+
+const Page2 = ({ selectedOptions, handleChange }) => {
+  const questions = [
+    {
+      id: "dreamCareer",
+      text: "What is your dream career field? (Select up to 4)",
+      options: [
+        "Consumer Electronics",
+        "Consumer Goods",
+        "Consumer Services",
+        "Cosmetics",
+        "Business",
+        "Education",
+        "Healthcare",
+        "Technology",
+      ],
+    },
+  ];
+
+  return (
+    <div>
+      {questions.map((question) => (
+        <OnboardingDropdown
+          key={question.id}
+          question={question.text}
+          options={question.options}
+          selectedOption={selectedOptions[question.id]}
+          onChange={(e) => handleChange(question.id, e.target.value)}
+        />
+      ))}
+    </div>
+  );
+};
+
+const Page3 = ({ selectedOptions, handleChange }) => {
+  const questions = [
+    {
+      id: "collegeDecision",
+      text: "Have you decided what college you will attend after highschool?",
+      options: ["Yes", "No"],
+    },
+    {
+      id: "collegeInterests",
+      text: "What colleges are you interested in attending after highschool?",
+      options: ["Harvard", "Yale", "UT Austin"],
+    },
+    {
+      id: "collegeAttending",
+      text: "What college will you be attending?",
+      options: ["Harvard", "Yale", "UT Austin"],
+    },
+  ];
+
+  return (
+    <div>
+      {questions.map((question) => (
+        <OnboardingDropdown
+          key={question.id}
+          question={question.text}
+          options={question.options}
+          selectedOption={selectedOptions[question.id]}
+          onChange={(e) => handleChange(question.id, e.target.value)}
+        />
+      ))}
+    </div>
+  );
+};
