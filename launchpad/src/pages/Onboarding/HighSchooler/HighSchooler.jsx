1fc6fc9e57ea4d245be827710f6128e2ca067067
gallo-json <gallojsantiago@gmail.com>
2024-07-04 09:13:53 -0500

    combined all questions into one dict

--- launchpad/src/pages/Onboarding/HighSchooler/HighSchooler.jsx
+++ launchpad/src/pages/Onboarding/HighSchooler/HighSchooler.jsx
@@ -2,8 +2,81 @@ import React, { useState } from 'react';
 import OnboardingDropdown from '../../../components/OnboardingDropdown/OnboardingDropdown';
 import ProgressBar from '../../../components/Progressbar/ProgressBar';
 
+const highSchoolQuestionsConfig = [
+  // Page 1
+  {
+      id: "whatSchool",
+      text: "What school do you go to?",
+      type: "select",
+      options: ["The Awty International School"],
+      page: 1,
+  },
+  {
+      id: "graduationYear",
+      text: "What year do you graduate?",
+      type: "select",
+      options: [1999, 2008],
+      page: 1,
+  },
+  {
+      id: "whatSection",
+      text: "Are you part of the French or International Section?",
+      type: "select",
+      options: ["French", "International"],
+      page: 1,
+  },
 
-export default function HighSchooler(){
+  // Page 2
+  {
+      id: "dreamCareer",
+      text: "What is your dream career field? (Select up to 4)",
+      type: "select",
+      options: [
+          "Consumer Electronics",
+          "Consumer Goods",
+          "Consumer Services",
+          "Cosmetics",
+          "Business",
+          "Education",
+          "Healthcare",
+          "Technology"
+      ],
+      page: 2,
+  },
+
+  // Page 3
+  {
+      id: "collegeDecision",
+      text: "Have you decided what college you will attend after highschool?",
+      type: "select",
+      options: ["Yes", "No"],
+      page: 3
+  },
+  {
+      id: "collegeInterests",
+      text: "What colleges are you interested in attending after highschool?",
+      type: "select",
+      options: [
+          "Harvard",
+          "Yale",
+          "UT Austin"
+      ],
+      page: 3  
+  },
+  {
+      id: "collegeAttending",
+      text: "What college will you be attending?",
+      type: "select",
+      options: [
+          "Harvard",
+          "Yale",
+          "UT Austin"
+      ],
+      page: 3  
+  }
+]
+
+export default function HighSchooler() {
   const numOfSections = 3;
   const [currentPage, setCurrentPage] = useState(1);
   const [selectedOptions, setSelectedOptions] = useState({});
@@ -18,11 +91,11 @@ export default function HighSchooler(){
   const renderPage = () => {
       switch (currentPage) {
       case 1:
-          return <Page1 selectedOptions={selectedOptions} handleChange={handleDropdownChange} />;
+          return <QuestionsPage selectedOptions={selectedOptions} handleChange={handleDropdownChange} pageNum={1} />;
       case 2:
-          return <Page2 selectedOptions={selectedOptions} handleChange={handleDropdownChange} />;
+          return <QuestionsPage selectedOptions={selectedOptions} handleChange={handleDropdownChange} pageNum={2} />;
       case 3:
-          return <Page3 selectedOptions={selectedOptions} handleChange={handleDropdownChange} />;
+          return <QuestionsPage selectedOptions={selectedOptions} handleChange={handleDropdownChange} pageNum={3} />;
       default:
           return null;
       }
@@ -41,95 +114,11 @@ export default function HighSchooler(){
   );
 };
   
-const Page1 = ({ selectedOptions, handleChange }) => {
-  const questions = [
-    {
-      id: "whatSchool",
-      text: "What school do you go to?",
-      options: ["The Awty International School"],
-    },
-    {
-      id: "graduationYear",
-      text: "What year do you graduate?",
-      options: [1999, 2008],
-    },
-    {
-      id: "whatSection",
-      text: "Are you part of the French or International Section?",
-      options: ["French", "International"],
-    },
-  ];
-
+const QuestionsPage = ({ selectedOptions, handleChange, pageNum }) => {
   return (
     <div>
-      {questions.map((question) => (
-        <OnboardingDropdown
-          key={question.id}
-          question={question.text}
-          options={question.options}
-          selectedOption={selectedOptions[question.id]}
-          onChange={(e) => handleChange(question.id, e.target.value)}
-        />
-      ))}
-    </div>
-  );
-};
-
-const Page2 = ({ selectedOptions, handleChange }) => {
-  const questions = [
-    {
-      id: "dreamCareer",
-      text: "What is your dream career field? (Select up to 4)",
-      options: [
-        "Consumer Electronics",
-        "Consumer Goods",
-        "Consumer Services",
-        "Cosmetics",
-        "Business",
-        "Education",
-        "Healthcare",
-        "Technology",
-      ],
-    },
-  ];
-
-  return (
-    <div>
-      {questions.map((question) => (
-        <OnboardingDropdown
-          key={question.id}
-          question={question.text}
-          options={question.options}
-          selectedOption={selectedOptions[question.id]}
-          onChange={(e) => handleChange(question.id, e.target.value)}
-        />
-      ))}
-    </div>
-  );
-};
-
-const Page3 = ({ selectedOptions, handleChange }) => {
-  const questions = [
-    {
-      id: "collegeDecision",
-      text: "Have you decided what college you will attend after highschool?",
-      options: ["Yes", "No"],
-    },
-    {
-      id: "collegeInterests",
-      text: "What colleges are you interested in attending after highschool?",
-      options: ["Harvard", "Yale", "UT Austin"],
-    },
-    {
-      id: "collegeAttending",
-      text: "What college will you be attending?",
-      options: ["Harvard", "Yale", "UT Austin"],
-    },
-  ];
-
-  return (
-    <div>
-      {questions.map((question) => (
+      {highSchoolQuestionsConfig.filter(question => question.page === pageNum)
+                .map((question) => (
         <OnboardingDropdown
           key={question.id}
           question={question.text}
