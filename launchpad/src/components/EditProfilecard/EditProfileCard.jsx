74b823cfc3cde764d3834b93925c9d5bf5600c54
dinglequandale <konradatittel@gmail.com>
2024-07-17 22:07:12 +0200

    inputs and selects restyled, word count functionality on text area enforced

--- launchpad/src/components/EditProfilecard/EditProfileCard.jsx
+++ launchpad/src/components/EditProfilecard/EditProfileCard.jsx
@@ -26,7 +26,7 @@ export default function EditProfileCard({userData}) {
     const navigate = useNavigate();
     const location = useLocation();
     // temporary data
-    const userType = "Alumni";
+    const userType = "Professional";
     const userName = "Shuja Gupta";
 
     const { currentUser } = useAuth();
