bd1e523c0f61601efcfc0f247f0a57c9c24279d7
gallo-json <gallojsantiago@gmail.com>
2024-08-03 16:37:05 -0500

    can search with three categories now, need to make it case insensitive

--- launchpad/src/services/searchServices.jsx
+++ launchpad/src/services/searchServices.jsx
@@ -10,13 +10,23 @@ const searchDocuments = async (collectionName, searchText) => {
     try {
       const docsRef = collection(db, collectionName);
   
-      const q = query(docsRef, orderBy('organizationName'), startAt(searchText), endAt(searchText + '\uf8ff'));
+      const q1 = query(docsRef, orderBy('organizationName'), startAt(searchText), endAt(searchText + '\uf8ff'));
+      const q2 = query(docsRef, orderBy('organizationType'), startAt(searchText), endAt(searchText + '\uf8ff'));
+      const q3 = query(docsRef, orderBy('organizationMission'), startAt(searchText), endAt(searchText + '\uf8ff'));
   
-      const snapshot = await getDocs(q);
+      const [snapshot1, snapshot2, snapshot3] = await Promise.all([
+        getDocs(q1),
+        getDocs(q2),
+        getDocs(q3),
+      ]);
   
-      const matches = snapshot.docs.map(doc => doc.data());
+      const matches = [
+        ...snapshot1.docs.map(doc => doc.data()),
+        ...snapshot2.docs.map(doc => doc.data()),
+        ...snapshot3.docs.map(doc => doc.data()),
+      ];
   
-      console.log(matches);
+      console.log([...new Set(matches)]);
     } catch (error) {
       console.error('Error searching Firestore: ', error);
     }
